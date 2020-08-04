def18c8a870b79450ddd8d25ec65e44b333817f5
jorgemonttess <68031573+jorgemonttess@users.noreply.github.com>
2020-08-04 15:44:41 -0500

    Update conexion_pg.php

--- includes/conexion_pg.php
+++ includes/conexion_pg.php
@@ -1,8 +1,8 @@
 <?
-    $dbhost = "localhost";
-    $dbuser = "postgres";
-    $dbpass = "circulo9";
-    $db = "triton";
+    $dbhost = "ec2-18-214-119-135.compute-1.amazonaws.com";
+    $dbuser = "lhowhjczqkvkmn";
+    $dbpass = "56bd792020d33b21f1bdfda7ff6b648dd354d9d23d8c7eaba2cd0562320db6fa";
+    $db = "db0ltmo0lk8joc";
     $port = "5432";
 
     $conexion_pg = "host=$dbhost port=$port dbname=$db user=$dbuser password=$dbpass";
